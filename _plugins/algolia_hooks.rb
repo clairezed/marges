cd6eadda6e4d91df9bd8a231cf7751c0637b2799
clairezed <clairezuliani@gmail.com>
2018-03-15 16:02:34 +0100

    Try to fix algolia search

--- _plugins/algolia_hooks.rb
+++ _plugins/algolia_hooks.rb
@@ -4,7 +4,6 @@ module Jekyll
       # Hook pour exlure les pages /tags et /lubies générées automatiquement
       def self.should_be_excluded?(filepath)
         # Do not index blog posts from 2015
-        p filepath
         return true if filepath =~ %r{lubies/}
         return true if filepath =~ %r{tags/}
         false
