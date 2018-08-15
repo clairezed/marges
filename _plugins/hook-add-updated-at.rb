053b11885e28a2ea9096ea78ece3f7883da4542f
clairezed <clairezuliani@gmail.com>
2018-08-15 08:11:21 +0200

    Suppression de toute référence à studies

--- _plugins/hook-add-updated-at.rb
+++ _plugins/hook-add-updated-at.rb
@@ -1,16 +1,3 @@
-Jekyll::Hooks.register :studies, :pre_render do |study|
-
-  # get the current post last modified time
-  modification_time = File.mtime( study.path )
-  puts study.path
-  puts modification_time
-
-  # inject modification_time in post's datas.
-  study.data['updated_at'] = modification_time
-
-end
-
-
 Jekyll::Hooks.register :posts, :pre_render do |post|
 
   # get the current post last modified time
